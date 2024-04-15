6f18da6fd373e6ab64f70650c0c2b0a7af592793
aussiearef <arefkr@gmail.com>
2024-04-16 06:10:35 +1000

    Added script for installation script of Alloy on Ubuntu

---
+++ alloy/install.sh
@@ -0,0 +1,11 @@
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+
+
+sudo apt-get update
+
+sudo apt-get install alloy
+
+
+
